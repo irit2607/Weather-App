068eddd00e320400c448199216b7948164382aa7
Irit Kushwaha <iritkushwaha2607@gmail.com>
2021-08-11 14:52:14 +0530

    weather_app

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -3,20 +3,20 @@ import { Card, Button, Container } from 'react-bootstrap';
 
 const Weather = () => {
     const [city, setCity] = useState('');
-    const [search, setSearch] = useState('mumbi')
+    const [search, setSearch] = useState('null')
     const inputEvent = (event) => {
         setSearch(event.target.value)
     }
 
     useEffect(() => {
         const fetchApi = async () => {
-            const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=734295c62d3d254be135ba69f420e7ec`;
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=734295c62d3d254be135ba69f420e7ec`;
             const response = await fetch(url);
             const resJson = await response.json();
             setCity(resJson.main)
         }
         fetchApi();
-    },[])
+    },[search])
 
     return (
         <div>
@@ -33,8 +33,7 @@ const Weather = () => {
                         </Card.Header>
                         <Card.Body>
                         {!city ? (
-                            <p>Data not found</p>
-                        ) : (
+                            <p>Data not found</p>) : (
                             <div>
                             <Card.Title>Location</Card.Title>
                             <i class="fa-solid fa-street-view"></i>{search}
@@ -44,12 +43,12 @@ const Weather = () => {
                             <Card.Text>
                                {city.temp_min} Cel | {city.temp_max} Cel
                             </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
+                            <Button variant="primary">Enjoy the Weather 🌤️</Button>
                             </div>
                         )}
                             
                         </Card.Body>
-                        <Card.Footer className="text-muted">2 days ago</Card.Footer>
+                        <Card.Footer className="text-muted">Made with ❤️ by Irit</Card.Footer>
                     </Card>
                 </Container>
             </>
